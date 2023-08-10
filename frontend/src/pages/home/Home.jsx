039ac54d8d42401d2c7f31011788bd994c1d8fd0
rashmi-rana <ranarashmi205@gmail.com>
2023-08-10 13:41:00 +0530

    changes iby Rashmi

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <div>home</div>
-      <div>querk</div>
+      <div> home </div>
+      <div> querk </div>
     </div>
   )
 }
